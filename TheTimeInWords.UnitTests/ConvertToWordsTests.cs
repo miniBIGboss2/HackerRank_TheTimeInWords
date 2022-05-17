97249d4227a3f6965f100c8d12168c9290592789
miniB <miniB@DESKTOP-CBOUGTP>
2022-05-17 14:53:48 +0300

    Add project files.

---
+++ TheTimeInWords.UnitTests/ConvertToWordsTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace TheTimeInWordsHackerRank.UnitTests
+{
+    [TestFixture]
+    internal class ConvertToWordsTests
+    {
+        [TestCase(3, 00, ExpectedResult = "three o' clock")]
+        [TestCase(5, 00, ExpectedResult = "five o' clock")]
+        [TestCase(5, 01, ExpectedResult = "one minute past five")]
+        [TestCase(5, 10, ExpectedResult = "ten minutes past five")]
+        [TestCase(5, 15, ExpectedResult = "quarter past five")]
+        [TestCase(5, 28, ExpectedResult = "twenty eight minutes past five")]
+        [TestCase(5, 30, ExpectedResult = "half past five")]
+        [TestCase(5, 32, ExpectedResult = "twenty eight minutes to six")]
+        [TestCase(5, 40, ExpectedResult = "twenty minutes to six")]
+        [TestCase(5, 45, ExpectedResult = "quarter to six")]
+        [TestCase(5, 47, ExpectedResult = "thirteen minutes to six")]
+        [TestCase(7, 15, ExpectedResult = "quarter past seven")]
+        public string ToWords_SampleInput_GetConverted(int h, int m) => 
+            ConvertToWords.ToWords(h, m);
+    }
+}
---
+++ TheTimeInWordsHackerRank/convertToWords.cs
@@ -0,0 +1,86 @@
+﻿namespace TheTimeInWordsHackerRank
+{
+    public static class ConvertToWords
+    {
+        public static string ToWords(int h, int m)
+        {
+            string temp;
+
+            string[] time = new string[]
+            {
+                "None",
+                "one",
+                "two",
+                "three",
+                "four",
+                "five",
+                "six",
+                "seven",
+                "eight",
+                "nine",
+                "ten",
+                "eleven",
+                "twelve",
+                "thirteen",
+                "fourteen",
+                "quarter",
+                "sixteen",
+                "seventeen",
+                "eighteen",
+                "nineteen",
+                "twenty",
+            };
+
+            switch (m)
+            {
+                case 00:
+                    temp = $"{time[h]} o' clock";
+                    break;
+
+                case 01:
+                    temp = $"one minute past {time[h]}";
+                    break;
+
+                case 30:
+                    temp = $"half past {time[h]}";
+                    break;
+
+                case <= 30:
+                    if (20 < m & m <= 29)
+                    {
+                        var numb = m / 10;
+                        temp = $"{time[numb * 10]} {time[m % 10]} minutes past {time[h]}";
+                        break;
+                    }
+
+                    if (m == 15)
+                    {
+                        temp = $"{time[m]} past {time[h]}";
+                        break;
+                    }
+
+                    temp = $"{time[m]} minutes past {time[h]}";
+                    break;
+
+                case > 30:
+                    if (30 < m & m <= 39)
+                    {
+                        var numb = (m / 10) - 1;
+                        temp = $"{time[numb * 10]} {time[10 - (m % 10)]} minutes to {time[h + 1]}";
+                        break;
+                    }
+
+                    if (m == 45)
+                    {
+                        temp = $"{time[60 - m]} to {time[h + 1]}";
+                        break;
+                    }
+
+                    temp = $"{time[60 - m]} minutes to {time[h + 1]}";
+                    break;
+            }
+
+            return temp;
+        }
+    }
+}
